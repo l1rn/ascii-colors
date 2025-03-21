e0c20f68de57f3e95afaeec9a9e0052d2c1ed88e
l1rn <ivtaks06@gmail.com>
2025-03-22 01:10:28 +0500

    fixes

--- color.c
+++ color.c
@@ -5,6 +5,7 @@
 void checkTextColor(char* x) {
 	int colorNumber;
 	x[strcspn(x, "\n")] = '\0';
+	
 	if(strcmp("red", x) == 0){
 		colorNumber = 31;
 		printf("text in your bash now red \e[%dm\n", colorNumber);
@@ -12,6 +13,22 @@ void checkTextColor(char* x) {
 	else if(strcmp("green", x) == 0){
 		colorNumber = 32;
 		printf("text now green \e[%dm\n", colorNumber);
+	}
+	else if(strcmp("yellow", x) == 0){
+		colorNumber = 33;
+		printf("text now yellow \e[%dm\n", colorNumber);
+	}
+	else if(strcmp("blue", x) == 0) {
+		colorNumber = 34; 
+		printf("text now blue \e[%dm\n", colorNumber);
+	}
+	else if(strcmp("magenta", x) == 0) { colorNumber = 35; printf("text now magenta \e[%dm\n", colorNumber);}
+	else if(strcmp("cyan", x) == 0) { colorNumber = 36; printf("text now cyan \e[%dm\n", colorNumber);}
+	else if(strcmp("white", x) == 0) { colorNumber = 37; printf("text now white \e[%dm\n", colorNumber);}
+	else if(strcmp("0", x) == 0) {
+		colorNumber = 0;
+		printf("\e[%dm", colorNumber);
+		exit(0);
 	}
 	else {
 		printf("\e[%0m");
@@ -30,19 +47,12 @@ void chooseColor(){
 	printf("|32 or green               	|\n");
 	printf("|33: yellow text               	|\n");
 	printf("|34: blue text                 	|\n");
-	printf("|35: mageanta text            	|\n");
+	printf("|35: magenta text            	|\n");
 	printf("|36: cyan text                 	|\n");
 	printf("|37: white text                 |\n");
 	printf("---------------------------------\n");		
 	fgets(txtColor, sizeof(txtColor), stdin);
 	checkTextColor(txtColor);
-	if(strcmp("red", txtColor) == -10){
-		colorNumber = 31;
-		printf("\e[%dm", colorNumber);	
-	}
-	// else if(strcmp("green", txtColor == -10){
-	//	colorNumber	
-	//}
 
 	printf("choose bg-color to change\n");
 	printf("---------------------------------\n");
